0fab3fc412177700d948dc76139d51f677bb84aa
Joao Victor <victorsrodrigues99@gmail.com>
2024-05-23 19:54:26 -0300

    Configurando entidade cliente

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -2,7 +2,6 @@ package br.com.projeto.api.controle;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
